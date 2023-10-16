d9aa4cc2289f2bda986d2acfda1478748f62e419
HwangSooHyeon <soohyeon062800@gmail.com>
2023-10-16 18:11:09 +0900

    test1

--- src/Router.js
+++ src/Router.js
@@ -3,12 +3,18 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import KimCodeLogin from './pages/KimCode/Login/Login';
 import KimCodeMain from './pages/KimCode/Main/Main';
 
+import Login from './pages/Hwangsoohyeon/Login/Login';
+import Join from './pages/Hwangsoohyeon/Join/Join';
+
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/kimcode-login" element={<KimCodeLogin />} />
         <Route path="/kimcode-main" element={<KimCodeMain />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/join" element={<Join />} />
       </Routes>
     </BrowserRouter>
   );
